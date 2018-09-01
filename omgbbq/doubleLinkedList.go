94002fc20d896a846277f1e8aab5b4abcfa0974b
John Ubante <condoti@yahoo.com>
2018-09-01 00:55:45 -0700

    My toy double linked list works....
    But not in tournament.
    
    Next:
    - SM singleton
    - SM players
    - persistent data file

--- omgbbq/doubleLinkedList.go
+++ omgbbq/doubleLinkedList.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+	"os"
+)
 
 type Thing struct {
 	name string
@@ -29,6 +33,83 @@ func (att *AllTheThings) addToEnd(t *Thing) {
 	att.things = append(att.things, t)
 }
 
+func (att AllTheThings) printLinkList(reverse bool, t *Thing) {
+	if t == nil {
+		t = att.things[0]
+	}
+	thing := *t
+
+	if reverse == false {
+		if thing.post == att.things[0] {
+			fmt.Println(thing.name)
+			return
+		}
+	} else {
+		if thing.pre == att.things[0] {
+			fmt.Println(thing.name)
+			return
+		}
+	}
+
+	if reverse == false {
+		fmt.Printf("%s -> ", thing.name)
+	} else {
+		fmt.Printf("%s <- ", thing.name)
+	}
+	time.Sleep(50 * time.Millisecond)
+
+	if reverse == false {
+		att.printLinkList(reverse, thing.post)
+	} else {
+		att.printLinkList(reverse, thing.pre)
+	}
+
+	return
+}
+
+// Position starts counting at 0 and ends at len(att.things)-1.
+func (att *AllTheThings) insert(position int, thing *Thing) {
+	length := len(att.things)
+	if position > length {
+		fmt.Println("You are trying to add to a position that is impossible.")
+		fmt.Println("The slice length is", length, "so the last thing is at position", length-1)
+		fmt.Println("so your position,", position, "is impossible.")
+		os.Exit(9)
+	}
+
+	// If the position is at the front of the slice, do this.
+	if position == 0 {
+		fmt.Println("Adding to the front.")
+		att.things[length-1].post = thing
+		thing.pre = att.things[length-1]
+		thing.post = att.things[0]
+		att.things[0].pre = thing
+		att.things = append([]*Thing{thing}, att.things...)
+		return
+	}
+
+	// If the position is at the end of the slice, do this.
+	if position == length {
+		fmt.Println("Adding to the end.")
+		att.things[length-1].post = thing
+		thing.pre = att.things[length-1]
+		thing.post = att.things[0]
+		att.things[0].pre = thing
+		att.things = append(att.things, thing)
+		return
+	}
+
+	// Otherwise, do this.
+	fmt.Println("Adding to position", position)
+	att.things[position-1].post = thing
+	thing.pre = att.things[position-1]
+	thing.post = att.things[position]
+	att.things[position].pre = thing
+
+	att.things = append(att.things[:position], append([]*Thing{thing}, att.things[position:]...)...)
+	return
+}
+
 func main() {
 	all := AllTheThings{nil}
 
@@ -40,6 +121,20 @@ func main() {
 	all.addToEnd(&t4)
 	t5 := Thing{"T5", nil, nil}
 	all.addToEnd(&t5)
+
+	all.printLinkList(false, nil)
+	all.printLinkList(true, nil)
+
+	t3 := Thing{"T3", nil, nil}
+	all.insert(2, &t3)
+	all.printLinkList(false, nil)
+	all.printLinkList(true, nil)
+
+	for i, thing := range all.things {
+		//fmt.Println(i, thing.name)
+		fmt.Printf("%d:              %s <- %s -> %s\n", i, thing.pre.name, thing.name, thing.post.name)
+		fmt.Printf("%p <- %p -> %p\n", thing.pre, thing, thing.post)
+	}
 
 	fmt.Println("==================")
 	a := Thing{"A", nil, nil}
