2935a104ddd174bc7d824415a4934a8a47e0bf8d
John Ubante <condoti@yahoo.com>
2018-10-15 23:35:40 -0700

    History singleton done
    Next is to write to the history.
    
    Next:
    - do something with bankroll
    - predictive player

--- sklanskyLoader.go
+++ sklanskyLoader.go
@@ -31,3 +31,23 @@ func main() {
 	fmt.Println("3. This is CK and C4:", score3.GetScoreOfHoleCardStrings("CK", "C4"))
 	fmt.Println("3. This is C4 and CK:", score3.GetScoreOfHoleCardStrings("C4", "CK"))
 }
+
+/*
+This will read in a SM model no more than once.
+Oh snap; reading in: poker/matrix/holeCardValues_SklanskyMalmuth.txt
+1. The filename is: poker/matrix/holeCardValues_SklanskyMalmuth.txt
+2. The filename is: poker/matrix/holeCardValues_SklanskyMalmuth.txt
+3. The filename is: poker/matrix/holeCardValues_SklanskyMalmuth.txt
+1. The filename is: new_filename
+2. The filename is: new_filename
+3. The filename is: new_filename
+1. This is AJ: 2
+1. This is J7: 8
+2. This is J7: 8
+3. This is SA and HQ: 3
+3. This is HQ and SA: 3
+3. This is HQ and HA: 2
+3. This is HQ and SQ: 1
+3. This is CK and C4: 7
+3. This is C4 and CK: 7
+ */
